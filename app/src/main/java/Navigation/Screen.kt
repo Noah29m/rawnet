95ba9957c5ea30c5384b17e68220246683b96eb1
HP <noahmuhindi11@gmail.com>
2023-01-10 19:08:20 +0300

    Added Navigation Routes

---
+++ app/src/main/java/Navigation/Screen.kt
@@ -0,0 +1,10 @@
+package Navigation
+
+sealed class Screen(val route:String){
+    object Dashboard :Screen("Dashboard")
+    object CheckOutProductScreen :Screen("Check_Out_Product_Screen")
+    object CheckOutScreen :Screen("Check_Out_Screen")
+    object PaymentScreen :Screen("Payment_Screen")
+    object PopularScreen :Screen("Popular_Screen")
+    object ProductDetailScreen :Screen("Product_Detail_Screen")
+}
